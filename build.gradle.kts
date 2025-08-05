99661e23e7f82a8fef1a49037b84363afa84bea8
yas-7 <kasmenai@gmail.com>
2025-08-05 11:18:29 +0300

    connect to db

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,21 @@
 plugins {
     id("java")
+    application
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass = "hexlet.code.Main"
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("com.h2database:h2:2.1.214")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
--- src/main/java/hexlet/code/Main.java
+++ src/main/java/hexlet/code/Main.java
@@ -1,7 +1,43 @@
 package hexlet.code;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    // Нужно указывать базовое исключение,
+    // потому что выполнение запросов может привести к исключениям
+    public static void main(String[] args) throws SQLException {
+        // Создаем соединение с базой в памяти
+        // База создается прямо во время выполнения этой строчки
+        // Здесь mem означает, что подключение происходит к базе данных в памяти,
+        // а hexlet_test — это имя базы данных
+        var conn = DriverManager.getConnection("jdbc:h2:mem:hexlet_test");
+
+        var sql = "CREATE TABLE users (id BIGINT PRIMARY KEY AUTO_INCREMENT, username VARCHAR(255), phone VARCHAR(255))";
+        // Чтобы выполнить запрос, создадим объект statement
+        Statement statement = conn.createStatement();
+        statement.execute(sql);
+        statement.close(); // В конце закрываем
+
+        var sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
+        Statement statement2 = conn.createStatement();
+        statement2.executeUpdate(sql2);
+        statement2.close();
+
+        var sql3 = "SELECT * FROM users";
+        Statement statement3 = conn.createStatement();
+        // Здесь вы видите указатель на набор данных в памяти СУБД
+        ResultSet resultSet = statement3.executeQuery(sql3);
+        // Набор данных — это итератор
+        // Мы перемещаемся по нему с помощью next() и каждый раз получаем новые значения
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("username"));
+            System.out.println(resultSet.getString("phone"));
+        }
+        statement3.close();
+
+        // Закрываем соединение
+        conn.close();
     }
 }
